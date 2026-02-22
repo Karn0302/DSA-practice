f2da5c0b2564ef0d49a476e904196467b9a2f590
Karn0302 <divyanshukarn2003@gmail.com>
2026-02-22 17:39:10 +0530

    linkedlist_basic_operations

---
+++ linked_list/LL_6_deleteAthead.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int data)
+    {
+        val=data;
+        next=NULL;
+    }
+};
+void insertAtHead(Node* &head,int val)
+{
+    Node* new_node = new Node(val);
+    new_node->next = head;
+    head= new_node;
+}
+
+void insertAtEND(Node* &head,int val)
+{
+    Node* new_node = new Node(val);
+    Node* temp=head;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=new_node;
+}
+
+void insertAtPosition(Node* &head, int val, int pos)
+{
+    if(pos==0)
+    {
+        insertAtHead(head,val);
+        return;
+    }
+    Node* temp=head;
+    Node* new_node= new Node(val);
+    int curr_pos=0;
+    while(curr_pos!=pos-1)
+    {
+        temp=temp->next;
+        curr_pos++;
+    }
+    new_node->next=temp->next;
+    temp->next=new_node;
+
+}
+
+void updateAtPosition(Node* &head,int k, int val)
+{
+     Node* temp= head;
+     int curr_pos = 0;
+     while(curr_pos!=k)
+     {
+        temp=temp->next;
+        curr_pos++;
+     }
+     //temp will be pointing to kth node
+     temp->val= val;
+}
+
+void deleteAtHead(Node* &head){
+    Node* temp = head; //node to be deleted 
+    head=head->next;
+    free(temp);
+}
+void display(Node* head)
+{
+    Node* temp= head;
+    while(temp!=NULL)
+    {
+        cout<<temp->val<<"->";
+        temp = temp->next;
+        
+    }
+    cout<<"NULL"<<endl;
+}
+int main()
+{
+    Node* head = NULL;
+    insertAtHead(head,1);
+    display(head);
+    insertAtHead(head,2);
+    display(head);
+    insertAtHead(head,3);
+    display(head);
+    insertAtEND(head,4);
+    display(head);
+    //if position value starts from 0
+    insertAtPosition(head,10,3);
+    display(head);
+    updateAtPosition(head,2,5);
+    display(head);
+
+    deleteAtHead(head);
+    display(head);
+    return 0;
+}
